36b04f087108b69df0f6271894e96cde57ef8521
andre.liang <54308739+namwoam@users.noreply.github.com>
2023-04-06 00:49:05 +0800

    fix carousel

--- pdao-website/src/lib/scenes/carousel.tsx
+++ pdao-website/src/lib/scenes/carousel.tsx
@@ -8,7 +8,7 @@ export default function CarouselPage()
     return (
             <Carousel 
             autoPlay={false}
-            sx={{height: '30rem', width: '30rem'}}
+            sx={{ width: "90%" }}
             indicatorIconButtonProps={{   // Move the buttons to the bottom. Unsetting top here to override default style.
                 style: {
                     display: 'none'
